7e1352d4a8b89a070f1292038d55f4736eab7116
Stefen Cabusas <scabusas@sg.ibm.com>
2017-07-22 21:09:45 +0800

    Added Add user api endpoint Added some user email validation

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,17 @@
 class ApiController < ApplicationController
   def addUser
+    @user = User.new()
+    if(params.has_key?(:email))
+      @user.email = params[:email]
+      if @user.save!
+        json_response({ message: 'User successfully created' }, :created)
+      else
+        logger.debug "Errors #{@user.errors}"
+        json_response({ error: 'Error saving user' }, :unprocessable_entity)
+      end
+    else
+      json_response({ error: 'Missing friends object'}, :bad_request)
+    end
   end
 
   def addFriend
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  has_many :relationships
+  attribute :email, :string
+  validates :email, :presence => true, uniqueness: true, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message => 'Invalid email format' }
+
+  has_many :relationships, dependent: :destroy 
+
 end
